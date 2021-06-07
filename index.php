e0bc38fd644809873f1889613ef691a61e69a33e
LucaLogallo <lucalogallo1998@gmail.com>
2021-06-07 18:33:50 +0200

    finito

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.
         
         Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.
-        
+
         Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.
         
         Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione."
@@ -35,20 +35,20 @@
       ],
       [
         'question' => "Perché il mio account è associato a un paese?",
-        'answer' => "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
+        'answer' => "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:<br>
 
         La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
             Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
             Google LLC, con sede negli Stati Uniti, per il resto del mondo.
-        La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+        La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.<br>
     
       Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account."
       ],
       [
         'question' => "Stabilire il paese associato al tuo account",
-        'answer' => "Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.
+        'answer' => "Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.<br>
 
-        I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.
+        I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.<br>
         
         Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato."
       ],
@@ -70,8 +70,6 @@
       "Domande frequenti"
     ]
   
-
-
   ?>
 
 
@@ -103,10 +101,13 @@
     <div class="container">
       <ul>
         <?php foreach($data as $dato){
+          $explode = explode(PHP_EOL, $dato['answer']);
           ?>
-        <li><?php echo $dato['question'] ?></li>
-        <li><?php echo $dato['answer'] ?></li>
-        <?php }?>
+        <li class="title"><?php echo $dato['question'] ?></li>
+        <?php foreach($explode as $p){?>
+        <li class="text"><?php echo $p ?></li>
+        
+        <?php }}?>
       </ul>
     </div>
   </main>
